1624955b256cdef5667a450de636de46c128eda5
JosefaSM <jsf.sepulveda@gmail.com>
2018-05-20 17:42:58 -0400

    Script llena tabla Estado

---
+++ BD/Insercion_Estado.sql
@@ -0,0 +1,5 @@
+--Insert para tabla Estado, estos son los 4 tipos de estado existentes
+insert into ESTADO values (1, 'no publicado', 'Programa no publicado');
+insert into ESTADO values (2, 'publicado', 'Programa publicado');
+insert into ESTADO values (3, 'No aprobado', 'Postulacion familia o alumno no aprobada');
+insert into ESTADO values (4, 'Aprobada', 'Postulacion familia o alumno no aprobada');
